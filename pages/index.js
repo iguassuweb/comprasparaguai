b8427515bfb97f7ab95dff298942fbea1eb9363d
diegolohan14 <diegolohan14@gmail.com>
2022-07-27 09:55:21 -0300

    test-repository

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { BodyContainer, Header, Footer } from "../src/components/layout";
 
 export default function Home() {
   return (
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Paraguai!</a>
+          Bem vindo ao <a href="https://nextjs.org">Paraguai!</a>
         </h1>
 
         <p className={styles.description}>
